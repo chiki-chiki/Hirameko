6dd15e266d73a7cab78f5126f4f509861fafa710
chiki-chiki <tsubame412@icloud.com>
2025-04-05 15:48:57 +0900

    localStorage保存

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,23 @@ function App() {
   
   
   const[ideas,setIdeas]=useState<Idea[]>([]);
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(()=>{
+    const savedIdeas=localStorage.getItem("ideas");
+    console.log("初回読み込み localStorage:", savedIdeas);
+    if(savedIdeas){
+      setIdeas(JSON.parse(savedIdeas));
+    }
+    setIsInitialized(true);
+  },[]);
+
+  useEffect(()=>{
+    //console.log("保存する ideas：",ideas);
+    if(isInitialized){
+      localStorage.setItem("ideas",JSON.stringify(ideas));
+    }
+  },[ideas, isInitialized]);
 
   const handleAddIdea=(idea:Omit<Idea,"createdAt">)=>{
     const newIdea:Idea={
@@ -26,17 +43,7 @@ function App() {
   const handleDeleteIdea=(indexToDelete:number)=>{
     setIdeas(ideas.filter((_,index)=>index!==indexToDelete));
   }
-  
-  useEffect(()=>{
-    const savedIdeas=localStorage.getItem("ideas");
-    if(savedIdeas){
-      setIdeas(JSON.parse(savedIdeas));
-    }
-  },[]);
-
-  useEffect(()=>{
-    localStorage.setItem("ideas",JSON.stringify(ideas));
-  },[ideas]);
+  //console.log("現在の ideas", ideas);
 
   return (
     <>
